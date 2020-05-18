ee35c63a189adb929e07f8d30ce05f33ef22f518
Yu-Chia <l850614@gmail.com>
2020-05-18 15:55:15 +0800

    babel

---
+++ src/person2.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(name="Default", age=0){
+        this.name = name;
+        this.age =age;
+    }
+    toJSON(){
+        return JSON.stringify({
+            name: this.name,
+            age: this.age,
+        })
+    }
+}
+
+console.log("exp01");
+export default Person;
---
+++ src/person_test2.js
@@ -0,0 +1,14 @@
+// const Person = require('./person');
+
+// const Person2 = require('./person');
+
+import Person from './person2';
+import Person2 from './person2';
+
+const p1 = new Person;
+const p2 = new Person("Martin",20);
+const p3  = new Person2;
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
