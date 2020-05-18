069b4b2f271ec9f7bc1734366ba689e44a614c4a
Yu-Chia <l850614@gmail.com>
2020-05-18 16:37:04 +0800

    04:36

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ app.get('/',function(req, res){
     res.send("STARBURST STREAM!!!");
 });
 
+app.use(express.static('public'));
+
+app.use((req, res) => {
+    res.type('text/html');
+    res.status(404);
+    res.send(`<h2>哭哭喔找不到頁面</h2>
+              <img src="https://steamuserimages-a.akamaihd.net/ugc/922556414965608331/89469F0D2C98046C6748CFFFAA5920C5A601A1E3/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true">`);
+})
+
 app.listen(3000, function(){
     console.log("server started");
 })
