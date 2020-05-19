e4e21a15c88e962623090e089d00ebe5e7d4c54a
Yu-Chia <l850614@gmail.com>
2020-05-19 17:23:22 +0800

    20200519 0523

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+//Top-level Middleware
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
+
 app.get('/',function(req, res){
     res.render('main', { name: 'Martin'});
 });
@@ -12,6 +17,31 @@ app.get('/sales',function(req, res){
     // res.json(data);
     res.render('sales-json',{ sales })
 });
+
+
+//middleware;
+
+app.get('/try-post-form', (req, res)=>{
+    res.render('try-post-form');
+})
+
+app.post('/try-post-form', (req, res)=>{
+    res.render('try-post-form', req.body);
+})
+
+app.post('/try-json-post', (req, res)=>{
+    req.body.contentType = req.get("Content-Type");
+    res.json(req.body);
+})
+
+// app.get('/ok',(req, res)=>{
+//     console.log(express);
+//     res.send("ok");
+// })
+
+app.get('/try-qs',function(req, res){
+    res.json(req.query);
+})
 
 app.get('/pending',function(req, res){
     
